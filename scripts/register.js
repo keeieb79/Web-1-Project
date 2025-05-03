8814643a817b73b4376e30b810bbbda9e10bce21
keeieb79 <keeieb79@gmail.com>
2025-05-03 19:42:31 +0300

    finishing logic of register and background image, but without DB

--- scripts/register.js
+++ scripts/register.js
@@ -1,16 +1,31 @@
-function check(e){
+document.getElementById('checkForm').onsubmit = function(e){
     let passwd = document.getElementById('passwd').value;
     let confPasswd = document.getElementById('passwdConf').value;
+    
+    let  chek = document.getElementById('agree').checked;
 
-    if(passwd !== confPasswd){
+    let age = document.getElementById('age').value;
+
+    if(passwd != confPasswd){
         window.alert('Password do not match');
+        document.getElementById('toRed1').style.color = 'red';
+        document.getElementById('toRed2').style.color = 'red';
+        
         e.preventDefault();
-        window.location.reload();
     }
 
-    let passwdLable = document.getElementsByClassName('toRed');
-    
+    if(chek !== true){
+        window.alert('You must agree to the terms.');
+        document.getElementById('check').style.color = 'red'
+        
+        e.preventDefault();
+    }
 
+    if(age < 10){
+        window.alert("You can't register bucz your age.");
+        e.preventDefault();
+    }
+    
 }
 
 function dispAge(){
